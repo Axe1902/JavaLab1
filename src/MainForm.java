b84c939bf58d4e99ee8c3623f381aafcfdcd6691
naukdankireev <naukdankireev@gmail.com>
2023-05-02 09:17:06 +0300

    Добавили коллекции

--- src/MainForm.java
+++ src/MainForm.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Vector;
 
@@ -19,6 +20,10 @@ public class MainForm extends JFrame {
     private JTextField TextField_Step;
     private JButton fillButton;
     private JButton clearButton;
+    private JButton saveButton;
+    private JButton loadButton;
+    private JButton serializationButton;
+    private JButton deserializationButton;
     private DefaultTableModel model;
 
     public LinkedList<RecIntegral> list = new LinkedList<>();
@@ -49,16 +54,16 @@ public class MainForm extends JFrame {
             public void actionPerformed(ActionEvent e)  {
 
                 try {
-                    if (Double.parseDouble(TextField_LowerLimit.getText()) > 1000000 ||
-                            Double.parseDouble(TextField_LowerLimit.getText()) < 0.000001)
-                    {
-                        throw new MyException("Выход за пределы диапазона", "Нижний лимит");
-                    }
-
                     if (Double.parseDouble(TextField_UpperLimit.getText()) > 1000000 ||
                             Double.parseDouble(TextField_UpperLimit.getText()) < 0.000001)
                     {
                         throw new MyException("Выход за пределы диапазона", "Верхний лимит");
+                    }
+
+                    if (Double.parseDouble(TextField_LowerLimit.getText()) > 1000000 ||
+                            Double.parseDouble(TextField_LowerLimit.getText()) < 0.000001)
+                    {
+                        throw new MyException("Выход за пределы диапазона", "Нижний лимит");
                     }
 
                     if (Double.parseDouble(TextField_Step.getText()) > 1000000 ||
@@ -67,7 +72,7 @@ public class MainForm extends JFrame {
                         throw new MyException("Выход за пределы диапазона", "Шаг");
                     }
 
-                    if (Double.parseDouble(TextField_Step.getText()) != 0)
+                    if (Double.parseDouble(TextField_Step.getText()) == 0)
                     {
                         throw new MyException("Шаг не может быть равен нулю", "Шаг");
                     }
@@ -151,6 +156,101 @@ public class MainForm extends JFrame {
                 for (int i = modelSize; i >= 0; i--)
                 {
                     model.removeRow(i);
+                }
+            }
+        });
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File file = new File("Data");
+                PrintWriter pw = null;
+                try {
+                    pw = new PrintWriter(file);
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+                for (RecIntegral itVar : list)
+                {
+                    pw.print(itVar.upLim.toString() + " ");
+                    pw.print(itVar.lowLim.toString() + " ");
+                    pw.print(itVar.st.toString() + " ");
+                    pw.print(itVar.res.toString() + " ");
+                    pw.print("\n");
+                }
+                pw.close();
+
+            }
+        });
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JFileChooser window = new JFileChooser(); //Создание JFileChooser с указанием директории пользователя по умолчанию
+                window.setCurrentDirectory(new File(".")); //Установка директории по умолчанию
+                window.setDialogTitle("Загрузка из файла"); //Заголовок окна
+                window.setMultiSelectionEnabled(false); //Невозможность выбора сразу нескольких файлов
+                window.setFileSelectionMode(JFileChooser.FILES_ONLY); //Выбор только файлов
+                window.setSelectedFile(new File("Data")); //Выбор файла по умолчанию
+                window.showDialog(window, "Загрузить из файла"); //Открытие окна выбора файла с настроенным наименованием кнопки
+
+                File MyFile = window.getSelectedFile(); //Чтение выделенного файла
+
+                int modelSize = model.getRowCount() - 1;
+                for (int i = modelSize; i >= 0; i--)
+                {
+                    model.removeRow(i);
+                }
+
+                try {
+                    FileReader myReader = new FileReader(MyFile); //Создание объекта класса FileReader для чтения из файла на основе имени файла в файловой системе
+                    BufferedReader buf = new BufferedReader(myReader); //BufferedReader записывает текст в поток вывода символов, буферизуя символы, чтобы обеспечить эффективную запись отдельных символов, массивов и строк
+
+                    Object[] lines = buf.lines().toArray(); //Метод lines() — метод, который возвращает поток строк, извлеченных из заданной многострочной строки
+
+                    for (int i = 0; i < lines.length; i++) {
+                        String[] row = lines[i].toString().split(" "); //Метод split разделяет строку на подстроки, используя разделитель, который определяется с помощью регулярного выражения
+                        model.addRow(new Object[]{Double.parseDouble(row[0]), Double.parseDouble(row[1]), Double.parseDouble(row[2]), Double.parseDouble(row[3])});
+                    }
+
+                    //Поток закрыт
+                    buf.close();
+                    myReader.close();
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        serializationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("RecInt.dat"));
+                    oos.writeObject(list);
+                    oos.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        });
+        deserializationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser window = new JFileChooser(); //Создание JFileChooser с указанием директории пользователя по умолчанию
+                window.setCurrentDirectory(new File(".")); //Установка директории по умолчанию
+                window.setDialogTitle("Загрузка из файла"); //Заголовок окна
+                window.setMultiSelectionEnabled(false); //Невозможность выбора сразу нескольких файлов
+                window.setFileSelectionMode(JFileChooser.FILES_ONLY); //Выбор только файлов
+                window.setSelectedFile(new File("RecInt.dat")); //Выбор файла по умолчанию
+                window.showDialog(window, "Загрузить из файла"); //Открытие окна выбора файла с настроенным наименованием кнопки
+
+                File MyFile = window.getSelectedFile(); //Чтение выделенного файла
+
+                try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(MyFile.getName()))) {
+                    list.clear();
+                    list = ((LinkedList<RecIntegral>)ois.readObject());
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
                 }
             }
         });
--- src/RecIntegral.java
+++ src/RecIntegral.java
@@ -1,8 +1,11 @@
-public class RecIntegral {
+import java.io.Serializable;
+
+public class RecIntegral implements Serializable {
     public Double upLim;
     public Double lowLim;
     public Double st;
     public Double res;
+    public int Size = 4;
 
     public RecIntegral(Double upLim, Double lowLim, Double st, Double res) {
         this.upLim = upLim;
